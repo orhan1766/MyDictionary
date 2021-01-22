650b1bfc32e80964b152e7f81d25fcef1f6442ea
orhan1766@gmail.com <orhan1766@gmail.com>
2021-01-22 22:28:45 +0300

    ilk deneme

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TKey[] tempKeys;
+
+        TValue[] values;
+        TValue[] tempValues;
+
+        public MyDictionary()
+        {
+            TKey[] keys = new TKey[0];
+            TValue[] values = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> _dictionary = new Dictionary<int, int>();
+
+            MyDictionary<int, string> _myDictionary = new MyDictionary<int, string>();
+            _myDictionary.Add(1, "orhan");
+            _myDictionary.Add(2, "fatih");
+            _myDictionary.Add(3, "emre");
+        }
+    }
+}
